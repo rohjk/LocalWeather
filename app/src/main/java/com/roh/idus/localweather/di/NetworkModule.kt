cf1afd1aba7a22c4036e02fe85afaf7cdb7cb2f6
JakeRoh <jake.roh@buzzvil.com>
2021-02-04 20:58:24 +0900

    provide http client

---
+++ app/src/main/java/com/roh/idus/localweather/di/NetworkModule.kt
@@ -0,0 +1,35 @@
+package com.roh.idus.localweather.di
+
+import android.os.Build
+import com.roh.idus.localweather.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesBucketPlaceRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder().client(okHttpClient)
+            .baseUrl(BuildConfig.BASE_HOST + "api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+    }
+
+}
