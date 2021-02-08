67031602bb59ee22a2cd0129b1ad452f3086cdd5
JakeRoh <jake.roh@buzzvil.com>
2021-02-08 12:22:41 +0900

    날씨 정보 없음 방어 로직 추가

--- app/src/main/java/com/roh/idus/localweather/localweather/adapter/WeatherInfoViewModel.kt
+++ app/src/main/java/com/roh/idus/localweather/localweather/adapter/WeatherInfoViewModel.kt
@@ -1,5 +1,6 @@
 package com.roh.idus.localweather.localweather.adapter
 
+import com.roh.idus.localweather.domain.model.Weather
 import com.roh.idus.localweather.domain.model.WeatherInfo
 
 class WeatherInfoViewModel constructor(
@@ -8,8 +9,16 @@ class WeatherInfoViewModel constructor(
 
     private val weathers = weaterInfo.weathers
 
+    private val defaultWeather = Weather (
+            id = 0,
+            icon = "",
+            state = "-",
+            humidity = 0,
+            temp = 0.0
+    )
+
     val localTitle = weaterInfo.locationTitle
-    val todayWeather = weathers[0]
-    val tomorowWeather = weathers[1]
+    val todayWeather = weathers.getOrNull(0) ?: defaultWeather
+    val tomorowWeather = weathers.getOrNull(1) ?: defaultWeather
 
 }
