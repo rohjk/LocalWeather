37a3a88e77aed074bd5615a731b4f6f43882c744
JakeRoh <jake.roh@buzzvil.com>
2021-02-09 10:12:19 +0900

    GetLocationWeatherUseCase Test 추가

---
+++ app/src/test/java/com/roh/idus/localweather/domain/usecase/GetLocationWeatherUseCaseTest.kt
@@ -0,0 +1,47 @@
+package com.roh.idus.localweather.domain.usecase
+
+import com.roh.idus.localweather.domain.model.LocationWeather
+import com.roh.idus.localweather.domain.repository.WeatherRepository
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.verify
+import io.reactivex.Observable
+import org.junit.Before
+import org.junit.Test
+
+
+class GetLocationWeatherUseCaseTest {
+
+    lateinit var getLocationWeatherUseCase: GetLocationWeatherUseCase
+
+    @MockK
+    lateinit var weatherRepository: WeatherRepository
+
+    @MockK
+    lateinit var locationWeather: LocationWeather
+
+    val id = 1234L
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+        getLocationWeatherUseCase = GetLocationWeatherUseCase(weatherRepository)
+    }
+
+    @Test
+    operator fun invoke() {
+        val expected = locationWeather
+
+        every { weatherRepository.getLocationWeather(id) } returns Observable.just(locationWeather)
+
+        getLocationWeatherUseCase(id).test()
+                .assertNoErrors()
+                .assertValue { it == expected }
+
+        verify(exactly = 1) {
+            weatherRepository.getLocationWeather(id)
+        }
+
+    }
+}
